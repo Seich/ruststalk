1cec2c8832c0cc1c384f0897b8a03d2cb55d2544
Sergio Díaz <seich@martianwabbit.com>
2015-01-04 16:45:50 -0600

    I am finally matching the full spec.

--- src/lib.rs
+++ src/lib.rs
@@ -12,29 +12,143 @@ impl Client {
 		}
 	}
 
-	pub fn put(&mut self, data: &str) -> Result<String, &str> {
-		write!(self.stream,
-			"put {pri} {delay} {ttr} {bytes}\r\n{data}\r\n", 
-			pri = 1i,
-			delay = 0i,
-			ttr = 0i,
-			bytes = data.len(),
-			data = data
-		);
+	fn run_command(&mut self, command: String) -> Result<String, &str> {
+		let _ = self.stream.write(command.as_bytes());
 
-		let mut buf = [0, ..4096];
-		let result = self.stream.read(&mut buf);
-		
+		let mut buffer = [0, ..4096];
+		let result = self.stream.read(&mut buffer);
+
 		match result {
-			Err(e) => fail!(e),
+			Err(e) => return Err(e.desc),
 			Ok(_) => {
-				let s = match str::from_utf8(buf) {
-					Some(e) => e.replace("\n", "").replace("\r", ""),
-					None => fail!("Invalid UTF-8 sequence"),
-				};
+				let s = str::from_utf8(buffer).unwrap();
 
 				return Ok(s.to_string());
 			}
 		}
 	}
+
+	// Producer commands
+	pub fn put(&mut self, priority: int, delay: int, ttr: int, data: &str) -> Result<String, &str> {
+		let cmd = format!("put {priority} {delay} {ttr} {bytes}\r\n{data}\r\n", 
+			priority = priority,
+			delay = delay,
+			ttr = ttr,
+			bytes = data.len(),
+			data = data
+		);
+
+		return self.run_command(cmd);
+	}
+
+	pub fn use_tube(&mut self, tube: &str) -> Result<String, &str> {
+		let cmd = format!("use {tube}\r\n", tube = tube);
+		return self.run_command(cmd);
+	}
+
+	// Worker commands
+	pub	fn reserve(&mut self) -> Result<String, &str> {
+		let cmd = "reserve\r\n";
+		return self.run_command(cmd.to_string());
+	}
+
+	pub	fn reserve_with_timeout(&mut self, seconds: int) -> Result<String, &str> {
+		let cmd = format!("reserve-with-timeout {seconds}\r\n", seconds=seconds);
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn delete(&mut self, id: int) -> Result<String, &str> {
+		let cmd = format!("delete {id}\r\n", id=id);
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn release(&mut self, id: int, priority: int, delay: int) -> Result<String, &str> {
+		let cmd = format!("release {id} {priority} {delay}\r\n", 
+			id=id, 
+			priority=priority, 
+			delay=delay
+		);
+
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn bury(&mut self, id: int, priority: int) -> Result<String, &str> {
+		let cmd = format!("bury {id} {priority}\r\n", 
+			id=id, 
+			priority=priority
+		);
+
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn touch(&mut self, id: int) -> Result<String, &str> {
+		let cmd = format!("touch {id}\r\n", id=id);
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn watch(&mut self, tube: &str) -> Result<String, &str> {
+		let cmd = format!("watch {tube}\r\n", tube=tube);
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn ignore(&mut self, tube: &str) -> Result<String, &str> {
+		let cmd = format!("ignore {tube}\r\n", tube=tube);
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn peek(&mut self, id: int) -> Result<String, &str> {
+		let cmd = format!("peek {id}\r\n", id=id);
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn peek_ready(&mut self) -> Result<String, &str> {
+		let cmd = format!("peek-ready\r\n");
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn peek_delayed(&mut self) -> Result<String, &str> {
+		let cmd = format!("peek-delayed\r\n");
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn peek_buried(&mut self) -> Result<String, &str> {
+		let cmd = format!("peek-buried\r\n");
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn kick(&mut self, bound: int) -> Result<String, &str> {
+		let cmd = format!("kick {bound}\r\n", bound=bound);
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn stats_job(&mut self, id: int) -> Result<String, &str> {
+		let cmd = format!("stats-job {id}\r\n", id=id);
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn stats_tube(&mut self, tube: &str) -> Result<String, &str> {
+		let cmd = format!("stats-tube {tube}\r\n", tube=tube);
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn stats(&mut self) -> Result<String, &str> {
+		let cmd = format!("stats\r\n");
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn list_tubes(&mut self) -> Result<String, &str> {
+		let cmd = format!("list-tubes\r\n");
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn list_tube_used(&mut self) -> Result<String, &str> {
+		let cmd = format!("list-tube-used\r\n");
+		return self.run_command(cmd.to_string());
+	}
+
+	pub fn list_tubes_watched(&mut self) -> Result<String, &str> {
+		let cmd = format!("list-tubes-watched\r\n");
+		return self.run_command(cmd.to_string());
+	}
+
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -3,12 +3,82 @@ use ruststalk::Client;
 use std::io::TcpStream;
 
 #[test]
-fn foo() {
+fn client_should_insert_new_jobs_into_the_queue() {
 	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
 	let mut c = Client::new(stream);
-	let p = c.put("Hello World");
 
-	if p.is_ok() {
-		println!("Inserted.");
-	}
+	assert!(c.put(1i, 0i, 0i, "Hello World").is_ok());
+}
+
+
+#[test]
+fn client_should_change_tube() {
+	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
+	let mut c = Client::new(stream);
+
+	assert!(c.use_tube("newTube").is_ok())
+}
+
+#[test]
+fn client_should_reserve_jobs() {
+	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
+	let mut c = Client::new(stream);
+
+	assert!(c.reserve().is_ok());
+}
+
+#[test]
+fn client_should_reserve_jobs_with_a_timeout() {
+	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
+	let mut c = Client::new(stream);
+
+	assert!(c.reserve_with_timeout(4).is_ok());
+}
+
+#[test]
+fn client_should_delete_jobs() {
+	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
+	let mut c = Client::new(stream);
+
+	assert!(c.delete(1).is_ok());
+}
+
+#[test]
+fn client_should_release_jobs() {
+	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
+	let mut c = Client::new(stream);
+
+	assert!(c.release(1, 1, 4).is_ok());
+}
+
+#[test]
+fn client_should_bury_jobs() {
+	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
+	let mut c = Client::new(stream);
+	
+	assert!(c.bury(1, 1).is_ok());
+}
+
+#[test]
+fn client_should_touch_jobs() {
+	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
+	let mut c = Client::new(stream);
+	
+	assert!(c.touch(1).is_ok());
+}
+
+#[test]
+fn client_should_watch_tubes() {
+	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
+	let mut c = Client::new(stream);
+	
+	assert!(c.watch("test").is_ok());
+}
+
+#[test]
+fn client_should_show_stats() {
+	let stream = TcpStream::connect("127.0.0.1", 11300).unwrap();
+	let mut c = Client::new(stream);
+	
+	assert!(c.stats().is_ok());
 }
